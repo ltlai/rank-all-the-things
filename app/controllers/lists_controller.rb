71ed830aac5384a96dba00179522a443b778f791
Leslie Lai <leslie.t.lai@gmail.com>
2014-07-17 15:50:15 -0700

    Abstract list population out of ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,11 +5,8 @@ class ListsController < ApplicationController
 
   def create
     @list = List.create!(name: params[:list][:name])
-    items = params[:list][:text_file].read
-
-    items.split("\n").each do |item|
-      @list.items.create!(name: item)
-    end
+    contents = params[:list][:text_file].read
+    @list.populate!(contents)
 
     redirect_to rank_list_path(@list)
   end
